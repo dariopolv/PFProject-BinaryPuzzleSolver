e315da8c81f87c12c2ed4d98ba58a5221bb279f4
dariopolv <dario.polverini93@gmail.com>
2019-09-20 15:58:06 +0200

    button spawn implementation, coordinates update, controls

--- main.lua
+++ main.lua
@@ -1,45 +1,113 @@
-require "buttons"
-function love.load() 
 
+
+
+function love.load() 
   local griglia = require "grid.griglia"
+  require "buttons" 
+  
+  mode6 = false
+  mode8 = false
+  mode10 = false
+  mode12 = false
+  
+  function resetControl()
+    mode6 = false
+    mode8 = false
+    mode10 = false
+    mode12 = false
+    end
 
   math.randomseed(os.time())
   math.random(); math.random(); math.random()
 
   font = love.graphics.newFont(32)
+  
+  function setvariables() 
+    if mode6 == true then
+    a = 6
+    b = 7.5
+    var = 0
+
+  elseif mode8 == true then
+    a = 8
+    b = 5.5
+    var = 50
+  elseif mode10 == true then
+    a = 10
+    b = 4.5
+    var = 145
+  elseif mode12 == true then
+    a = 12
+    b = 3.5
+    var = -490
+  end
+end
 
   table.insert(buttons, newButton("6 x 6 Mode", 
-
+      
       function() 
-        mode(6, 7.5)
-        draw_buttons(6, 7.5, 500, 50, 535, 55)
-      end))
+      mode6 = true
+      setvariables()
+      mode(a, b)
+      draw_buttons(a, b, var)
+         
+    end))
 
   table.insert(buttons, newButton("8 x 8 Mode",
       function() 
-        mode(8, 5.5)
-        draw_buttons(8, 5.5,550,50,585,55)
-      end))
+        
+        mode8 = true
+        setvariables()
+        mode(a,b)
+        draw_buttons(a,b,var)
+     
+        end))
 
   table.insert(buttons, newButton("10 x 10 Mode",
       function() 
-        mode(10, 4.5)
-        draw_buttons(10, 4.5, 645, 50, 680, 55)
+        mode10 = true
+        setvariables()
+        mode(a,b)
+        draw_buttons(a,b,var)
       end))
 
   table.insert(buttons, newButton("12 x 12 Mode",
       function() 
-        mode(12, 3.5)
-        draw_buttons(12, 3.5, 10, 50, 40, 55)
+        mode12 = true
+        setvariables()
+        mode(a,b)
+        draw_buttons(a,b,var)
       end))
 
   table.insert(buttons, newButton("Quit", function() love.event.quit(0) end))
+  
+  
+  table.insert(buttons2, newButton("Back", function() 
+              function love.draw()
+                love.graphics.setBackgroundColor(0,0,0,0)
+                resetControl()
+                generate_buttons(buttons)
+                end
+      end))
+        
+        table.insert(buttons2, newButton("Solve", function() 
+              
+              function love.draw() 
+                setvariables()
+                drawTables(a,b)
+              end
+            end
+            ))
+        
+        table.insert(buttons2, newButton("Refresh", function()
+               setvariables()
+               mode(a,b)
+               draw_buttons(a,b,var)
+              end))
 
 end
 
-
 function love.draw()
-
   generate_buttons(buttons)
 
 end
